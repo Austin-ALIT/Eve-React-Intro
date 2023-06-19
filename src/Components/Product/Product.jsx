cfb65c0c28e76ccdc4a387e11c38b9231af7af0a
Austin-ALIT <austin@al.co.za>
2023-06-19 15:11:19 +0200

    Added object destructuring.

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,14 +1,17 @@
 import "../../Styles/product.css"
 
 export default function Product(props){
-    let prodObj = props.product;
+    const {'product': prodName, 
+        'price': prodPrice, 
+        'description': prodDesc
+    } = props.product;
 
     let classes = "clsProd " + props.className;
 
     return(
         <div className = {classes}>
-            <p> Product {prodObj['product']}, price {prodObj['price']}</p>
-            <p> Description: {prodObj['description']}</p>
+            <p> Product {prodName}, price {prodPrice}</p>
+            <p> Description: {prodDesc}</p>
         </div>
     );
 }
